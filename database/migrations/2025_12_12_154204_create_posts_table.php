e24a710a83cff8c8c50826bc2bc693874ac0bc8e
Khen D. Sorela <159198973+BlackShark404@users.noreply.github.com>
2025-12-13 00:01:25 +0800

    add columns to posts table: title, slug, content, and softDeletes

--- database/migrations/2025_12_12_154204_create_posts_table.php
+++ database/migrations/2025_12_12_154204_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
