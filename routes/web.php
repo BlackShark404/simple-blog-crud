5ccf28455f6c1df0f36341c89ad48e647e187e5d
Khen D. Sorela <159198973+BlackShark404@users.noreply.github.com>
2025-12-15 18:51:05 +0800

    fix route naming for posts index and store methods

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index'])->name('post.index');
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::post('/post', [PostController::class, 'store'])->name('posts.store');
