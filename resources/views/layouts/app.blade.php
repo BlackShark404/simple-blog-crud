0f37241160735a274c0aa78e196f8db16b3b5948
Khen D. Sorela <159198973+BlackShark404@users.noreply.github.com>
2025-12-15 18:51:37 +0800

    refactor app layout to use yield for title and content sections

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="{{ \Illuminate\Support\Facades\Cookie::get('app_theme', $defaultTheme) }}">
+<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="turbo-cache-control" content="no-preview">
-        <title>{{ $title ?? 'Simple Blog' }}</title>
+        
+        <title>@yield('title', config('app.name'))</title>
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        {{ $slot }}
+        @yield('content')
     </body>
 </html>
